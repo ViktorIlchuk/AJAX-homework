cb58a9692e0ef4f09ebb7eecaed32c0f5c2af206
Victor Ilchuk <ilchuk710@gmail.com>
2019-04-27 22:43:56 +0300

    fixed bug in func deleteUsers

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const API = 'https://test-users-api.herokuapp.com/';
+const button = document.querySelector("#create-user-btn");
 
 const getUsers = () => {
     return fetch(API + 'users').then(res => {
         return res.json();
+        console.log(res.json());
     }).catch(err => {
         console.log('couldnt find users', err);
         return [];
@@ -11,7 +13,14 @@ const getUsers = () => {
 
 const deleteUser = async (userId, userElement) => {
     try {
-        const res = await fetch (API + 'users/' + userId, {method: 'DELETE'});
+        const res = await fetch (API + 'users/' + userId, {
+            method: 'DELETE',
+            body: JSON.stringify({name, age}),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+              }        
+        });
         userElement.remove();
     } catch(err) {
         console.log('couldnt delete user', err);
@@ -70,9 +79,23 @@ const createUser = () => {
       })
 };
 
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    const name = document.querySelector("#name");
+    const age = document.querySelector("#age");
+    const warning = "Name must consist of English letter, age must consist of numbers.";
+    const attention = "Plese enter your name and age.";
     const createUserBtn = document.querySelector('#create-user-btn')
-    createUserBtn.addEventListener('click', createUser);
+    document.querySelector("#message").innerHTML = attention;
+    
+    
+    if (isNaN(age.value) || age.value < 0 ) {
+        document.querySelector("#message").innerHTML = warning;
+    } else {
+        createUserBtn.addEventListener('click', createUser);
+    }
   });
 
   init();
